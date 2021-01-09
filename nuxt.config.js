dd88d6faa916a91678f72db2345f394481593be2
hiroki <raxito2008@yahoo.co.jp>
2021-01-09 23:36:15 +0900

    fix: Sass使用準備 ress追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,8 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
+    'ress',
+    '~/assets/scss/style.scss'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
